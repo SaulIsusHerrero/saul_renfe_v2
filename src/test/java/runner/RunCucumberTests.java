6ae9438e7ac5c7fbf2633712c18e5ecf6bb6752c
Elia pizarro <elia.pizarrosegura@amaris.com>
2025-04-21 20:51:55 +0200

    mas cambios, sigue sin correr con mvn pero si con el plugin

--- src/test/java/runner/RunCucumberTests.java
+++ src/test/java/runner/RunCucumberTests.java
@@ -6,7 +6,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue={"steps"}, features = "src/test/resources/features", plugin = { "pretty", "html:target/site/cucumber-pretty",
-		"json:target/cucumber.json" })
+@CucumberOptions(
+    features = "src/test/resources/features", // Ruta a los archivos .feature
+    glue = "steps", // Paquete donde están los Step Definitions
+    plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
+    monochrome = true
+)
 public class RunCucumberTests {
+    // Clase vacía, solo sirve como punto de entrada para Cucumber
 }
